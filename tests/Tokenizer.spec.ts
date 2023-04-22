556fdf50225cf098bed18fa013840c7ca76ee47f
HubertKuch <kuchhubert@gmail.com>
2023-04-22 12:47:55 +0200

    Tested tokenizing enums

--- tests/Tokenizer.spec.ts
+++ tests/Tokenizer.spec.ts
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import {Token} from "../src/helpers";
+import {Token, TokenType} from "../src/helpers";
 import Tokenizer from "../src/Tokenizer";
 
 describe("Tokenizer test", () => {
@@ -22,5 +22,33 @@ describe("Tokenizer test", () => {
 
         expect(tokens).to.be.not.empty;
         expect(Object.values(tokens[0][0])).is.a('array').satisfy((val) => val);
+    });
+
+    it('should tokenize enum with valid cases', function () {
+        const source: string = "enum UserRole { USER ADMIN }";
+
+        const tokens :Token[][] = Tokenizer.tokenize(source);
+
+        expect(tokens).to.be.not.null;
+        expect(tokens[0][3].value).to.be.eq("USER");
+        expect(tokens[0][4].value).to.be.eq("ADMIN");
+    });
+
+    it('should tokenize enum no cases', function () {
+        const source: string = "enum UserRole {}";
+
+        const tokens :Token[][] = Tokenizer.tokenize(source);
+
+        expect(tokens).to.be.not.empty;
+    });
+
+    it('should tokenize enum with semicolon', function () {
+        const source: string = "enum UserRole { USER; ADMIN; }";
+
+        const tokens :Token[][] = Tokenizer.tokenize(source);
+
+        expect(tokens).to.be.not.empty;
+        expect(tokens[0][3].value).to.be.eq("USER");
+        expect(tokens[0][5].value).to.be.eq("ADMIN");
     });
 })
